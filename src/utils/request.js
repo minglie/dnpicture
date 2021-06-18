97a2feb61f6ea4bd1a9518727b76cdfafc86aca4
minglie <934031452@qq.com>
2021-06-19 04:01:47 +0800

    aa

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,4 @@
-export default (params) => {
+let request = (params) => {
   uni.showLoading({
     title: '加载中',
   });
@@ -17,4 +17,43 @@ export default (params) => {
       },
     });
   });
+};
+
+export default request;
+
+
+
+const get = async (api, params = {}, headers) => {
+  const res = await request({
+    url: api,
+    data: params,
+    header: headers || {},
+    method: "GET"
+  });
+  return res;
+};
+
+const post = async (api, params = {}, headers) => {
+  const res = request({
+    url: api,
+    data: params,
+    header: headers || {},
+    method: "POST"
+  });
+  return res;
+};
+
+//将ajax请求挂到全局对象M上
+M.request = {}
+M.request.get = get;
+M.request.post = post;
+M.request.reqByReq = async (url, req) => {
+  if (req.method == "get") {
+    let result = await get(url, req.params, req.headers)
+    return result;
+  }
+  if (req.method == "post") {
+    let result = await post(url, req.params, req.headers)
+    return result;
+  }
 };
